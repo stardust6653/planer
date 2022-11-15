ebb791e187291a57c97b3921c777eb1b27b88d02
stardust6653 <stardust6653@gmail.com>
2022-11-16 01:17:08 +0900

    Style: Reset CSS 적용
    - styled-reset을 활용한 reset css 적용

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { Reset } from "styled-reset";
 import App from "./App";
 
 const container = document.getElementById("root");
@@ -7,6 +8,7 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
+    <Reset />
     <App />
   </React.StrictMode>
 );
